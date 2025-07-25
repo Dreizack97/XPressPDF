43bad96ae6bdf7ea256cd88ac916f79b66ab54f9
Dreizack97 <dreizack97@gmail.com>
2025-07-25 12:58:12 -0600

    (BLL) Desarrollo de interfaz e implementación de 'EmailService'.

---
+++ BLL/Implementation/EmailService.cs
@@ -0,0 +1,63 @@
+﻿using BLL.Interfaces;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace BLL.Implementation
+{
+    public class EmailService : IEmailService
+    {
+        private struct MailServer
+        {
+            public const string ADDRESS = "";
+
+            public const string PASSWORD = "";
+
+            public const string DISPLAY_NAME = "";
+
+            public const string HOST = "";
+
+            public const int PORT = 587;
+
+            public const bool SSL = true;
+        }
+
+        public async Task<bool> SendEmailAsync(string addresses, string subject, string body, List<string>? attachments = null)
+            => await SendEmailAsync(new List<string> { addresses }, subject, body, attachments);
+
+        public async Task<bool> SendEmailAsync(List<string> addresses, string subject, string body, List<string>? attachments = null)
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress(MailServer.DISPLAY_NAME, MailServer.ADDRESS));
+
+            foreach (string address in addresses)
+                message.To.Add(MailboxAddress.Parse(address));
+
+            message.Subject = subject;
+
+            BodyBuilder builder = new BodyBuilder { HtmlBody = body };
+
+            if (attachments != null)
+                foreach (string attachment in attachments)
+                    if (!string.IsNullOrWhiteSpace(attachment))
+                        builder.Attachments.Add(attachment);
+
+            message.Body = builder.ToMessageBody();
+
+            using (SmtpClient smtpClient = new SmtpClient())
+            {
+                try
+                {
+                    await smtpClient.ConnectAsync(MailServer.HOST, MailServer.PORT, MailServer.SSL);
+                    await smtpClient.AuthenticateAsync(MailServer.ADDRESS, MailServer.PASSWORD);
+                    await smtpClient.SendAsync(message);
+                    await smtpClient.DisconnectAsync(true);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ BLL/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿namespace BLL.Interfaces
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmailAsync(string addresses, string subject, string body, List<string>? attachments = null);
+
+        Task<bool> SendEmailAsync(List<string> addresses, string subject, string body, List<string>? attachments = null);
+    }
+}
