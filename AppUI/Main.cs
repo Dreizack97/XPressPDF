8168314ed812e60b349aa67983faeebe39e8c56a
Dreizack97 <dreizack97@gmail.com>
2025-07-25 08:07:22 -0600

    Creación de proyecto GUI.

---
+++ AppUI/Main.cs
@@ -0,0 +1,89 @@
+using AppUI.Objetcs;
+
+namespace AppUI
+{
+    public partial class Main : Form
+    {
+        private List<XmlFile> xmlFiles = new List<XmlFile>();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*",
+                FilterIndex = 1,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                Multiselect = true,
+                RestoreDirectory = true,
+                Title = "Select XML files"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    dataGrid.DataSource = null;
+                    xmlFiles.Clear();
+
+                    foreach (string file in openFileDialog.FileNames)
+                    {
+                        FileInfo fileInfo = new FileInfo(file);
+
+                        xmlFiles.Add(new XmlFile
+                        {
+                            Name = fileInfo.Name,
+                            Path = fileInfo.FullName,
+                            Size = (fileInfo.Length / 1024) + " KB",
+                            ModificationDate = fileInfo.LastWriteTime
+                        });
+                    }
+
+                    dataGrid.DataSource = xmlFiles;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private async void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (xmlFiles.Count == 0)
+            {
+                MessageBox.Show("You have not selected any files to convert. Please select at least one file to continue.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            int i = 0;
+            btnConvert.Enabled = false;
+
+            try
+            {
+                await Task.Run(async () =>
+                {
+                    foreach (XmlFile xmlFile in xmlFiles)
+                    {
+                        dataGrid.Rows[i].Cells[4].Value = "Complete";
+                        i++;
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                dataGrid.Rows[i].Cells[4].Value = ex.Message;
+            }
+            finally
+            {
+                btnConvert.Enabled = true;
+            }
+        }
+    }
+}
---
+++ AppUI/Objetcs/XmlFile.cs
@@ -0,0 +1,15 @@
+﻿namespace AppUI.Objetcs
+{
+    public class XmlFile
+    {
+        public string Name { get; set; } = null!;
+
+        public string Path { get; set; } = null!;
+
+        public string Size { get; set; } = null!;
+
+        public DateTime ModificationDate { get; set; }
+
+        public string Status { get; set; } = "Pending";
+    }
+}
