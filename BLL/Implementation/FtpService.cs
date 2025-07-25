fa36f4b08022b31cbc501be34ed004f890b2a28a
Dreizack97 <dreizack97@gmail.com>
2025-07-25 11:09:34 -0600

    (BLL) Desarrollo de interfaz e implementaciónd de 'FtpService'.

---
+++ BLL/Implementation/FtpService.cs
@@ -0,0 +1,69 @@
+﻿using BLL.Interfaces;
+using BLL.Objetcs;
+using FluentFTP;
+using FluentFTP.Exceptions;
+
+namespace BLL.Utilities
+{
+    public class FtpService : IFtpService
+    {
+        private readonly FtpClient ftpClient;
+
+        private struct FtpServer
+        {
+            public const string HOST = "";
+            public const string USER = "";
+            public const string PASSWORD = "";
+            public const int PORT = 21;
+            public const string ROOTPATH = "";
+        }
+
+        public FtpService()
+        {
+            ftpClient = new FtpClient(FtpServer.HOST, FtpServer.USER, FtpServer.PASSWORD, FtpServer.PORT);
+        }
+
+        public List<FileUploadResult> Upload(string filePath, string uploadPath)
+            => Upload([filePath], uploadPath);
+
+        public List<FileUploadResult> Upload(string[] filesPath, string uploadPath)
+        {
+            ArgumentNullException.ThrowIfNull(filesPath);
+            ArgumentNullException.ThrowIfNull(uploadPath);
+
+            try
+            {
+                ftpClient.Connect();
+                List<FileUploadResult> results = new List<FileUploadResult>();
+
+                foreach (string filePath in filesPath)
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    string remotePath = Path.Combine(FtpServer.ROOTPATH, uploadPath, fileName);
+
+                    FtpStatus status = ftpClient.UploadFile(filePath, remotePath, FtpRemoteExists.Overwrite, true);
+
+                    if (status == FtpStatus.Success)
+                        results.Add(new FileUploadResult(filePath, remotePath, true));
+                    else
+                        results.Add(new FileUploadResult(filePath, remotePath, false, $"FTP upload failed. Status: {status}"));
+                }
+
+                return results;
+            }
+            catch (FtpException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (ftpClient.IsConnected)
+                    ftpClient.Disconnect();
+            }
+        }
+    }
+}
---
+++ BLL/Interfaces/IFtpService.cs
@@ -0,0 +1,11 @@
+﻿using BLL.Objetcs;
+
+namespace BLL.Interfaces
+{
+    public interface IFtpService
+    {
+        List<FileUploadResult> Upload(string filePath, string uploadPath);
+
+        List<FileUploadResult> Upload(string[] filesPath, string uploadPath);
+    }
+}
