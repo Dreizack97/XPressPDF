d654299af7773e44865a5c9cfb4423162a4e91b0
Dreizack97 <dreizack97@gmail.com>
2025-07-25 11:05:53 -0600

    (BLL) Creación de 'FileUploadResult'.

---
+++ BLL/Objects/FileUploadResult.cs
@@ -0,0 +1,21 @@
+﻿namespace BLL.Objetcs
+{
+    public class FileUploadResult
+    {
+        public string FilePath { get; }
+
+        public string RemotePath { get; }
+
+        public bool Success { get; }
+
+        public string? ErrorMessage { get; }
+
+        public FileUploadResult(string filePath, string remotePath, bool success, string? errorMessage = null)
+        {
+            FilePath = filePath;
+            RemotePath = remotePath;
+            Success = success;
+            ErrorMessage = errorMessage;
+        }
+    }
+}
