3c09496ccb6a5415da00496cd85b83dfca306d0f
Dreizack97 <dreizack97@gmail.com>
2026-06-11 18:26:05 -0600

    fix(schemas): corregir nombres camelCase de atributos del complemento ValesDeDespensa
    Los atributos del XSD del SAT (version, tipoOperacion, registroPatronal,
    numeroDeCuenta, total) son camelCase; sin el nombre explícito en
    XmlAttribute el complemento nunca se deserializaba.

--- Schemas/Complements/ValesDeDespensa/ValesDeDespensa.cs
+++ Schemas/Complements/ValesDeDespensa/ValesDeDespensa.cs
@@ -11,19 +11,19 @@ namespace Schemas.Complements.ValesDeDespensa
     [XmlRoot(Namespace = "http://www.sat.gob.mx/valesdedespensa", IsNullable = false)]
     public partial class ValesDeDespensa
     {
-        [XmlAttribute]
+        [XmlAttribute("version")]
         public string Version { get; set; } = "1.0";
 
-        [XmlAttribute]
+        [XmlAttribute("tipoOperacion")]
         public string TipoOperacion { get; set; } = "monedero electrónico";
 
-        [XmlAttribute]
+        [XmlAttribute("registroPatronal")]
         public string RegistroPatronal { get; set; }
 
-        [XmlAttribute]
+        [XmlAttribute("numeroDeCuenta")]
         public string NumeroDeCuenta { get; set; }
 
-        [XmlAttribute]
+        [XmlAttribute("total")]
         public decimal Total { get; set; }
 
         public ValesDeDespensaConceptos Conceptos { get; set; }
